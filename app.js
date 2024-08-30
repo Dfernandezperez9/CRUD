e7fef0b6afb69678f1f7192014525fb36a7396e0
David <dfernandezperez9@gmail.com>
2024-08-30 17:25:02 +0200

    Fin

---
+++ app.js
@@ -0,0 +1,28 @@
+const EXPRESS = require("express");
+const APP = EXPRESS();
+const ROUTE = require("./ruta");
+const DATA = require("./data");
+const PORT = 3000;
+
+
+APP.use(EXPRESS.urlencoded({ extended: true }));
+
+APP.use("/", ROUTE);
+APP.use("/usuarios", ROUTE);
+
+APP.post('/eliminar/:nombre', (req, res) => {
+    const NOMBRE = req.params.nombre;
+    const _METHOD = req.body._method;
+    if (_METHOD === 'DELETE') {
+        res.send(`
+        <h1>Usuario ${NOMBRE} eliminado con éxito</h1>
+        <a href="/">Volver</a>
+        `);
+    } else {
+        res.status(405).send('Método no permitido');
+    }
+});
+
+APP.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
---
+++ ruta.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const APP = express();
+const ROUTE = express.Router();
+const DATA = require("./data");
+const USUARIOS = DATA.usuarios;
+
+
+ROUTE.get("/", (req, res) => {
+    res.send(`
+        <h1>Lista de usuarios</h1>
+        <ul>
+            ${USUARIOS.map((usuario) => `
+                <li>
+                    <form action="/eliminar/${usuario.nombre}" method="POST">
+                        <input type="hidden" name="_method" value="DELETE"> <button type="submit">Eliminar</button>
+                        ID: ${usuario.id} | Nombre: ${usuario.nombre}
+                    </form>
+                </li>
+            `).join("")}
+        </ul>
+        <form action="/usuarios" method="POST">
+            <label for="nombre">Nombre</label>
+            <input type="text" id="nombre" name="nombre" required>
+            <button type="submit">Agregar usuario</button>
+            <a href="/usuarios">Usuarios</a>
+        </form>
+    `);
+});
+
+
+
+ROUTE.get("/usuarios", (req, res) => {
+	res.json(USUARIOS);
+});
+
+ROUTE.post("/usuarios", (req, res) => {
+	const NUEVO_USUARIO = {
+		id: USUARIOS.length + 1,
+		nombre: req.body.nombre,
+	};
+    USUARIOS.push(NUEVO_USUARIO);
+	res.redirect("/");
+});
+
+module.exports = ROUTE;
